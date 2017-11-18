c64606f0cd4082827ad0bc6b5b94e0b53cd8ca50
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-18 19:38:50 +0300

    Added EndCallEventArgs

---
+++ Contracts/CustomArgs/EndCallEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.CustomArgs
+{
+    public class EndCallEventArgs: EventArgs
+    {
+        public string ReceivingNumber { get; private set; }
+        public string CallerNumber { get; private set; }
+
+        public EndCallEventArgs(string caller, string reciver)
+        {
+            CallerNumber = caller;
+            ReceivingNumber = reciver;
+        }
+    }
+}
