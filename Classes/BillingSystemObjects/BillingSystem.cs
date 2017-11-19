bca4e5398b97381fb3334546a03ce0dfd602ecf1
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-19 20:02:21 +0300

    Added Billing system class

---
+++ Classes/BillingSystemObjects/BillingSystem.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.BillingSystemObjects
+{
+    public class BillingSystem : IBillingSystem
+    {
+        public List<IContract> Contracts => throw new NotImplementedException();
+    }
+}
