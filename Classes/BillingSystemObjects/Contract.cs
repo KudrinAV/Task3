c5755a962a35a64908f8bd4be20bf855fb104de1
Антон Кудрин <antonkudrin25@gmail.com>
2017-11-19 20:02:59 +0300

    Added Contract class

---
+++ Classes/BillingSystemObjects/Contract.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.BillingSystemObjects
+{
+    public class Contract : IContract
+    {
+        public int IdOfPort => throw new NotImplementedException();
+
+        public ITariffPlan Tariff => throw new NotImplementedException();
+    }
+}
